395b542b8afaaf9fae58c27513c8ed66d6f4edbb
oleksandr_stetsyshyn <56292371+oleksandrstetsyshyn@users.noreply.github.com>
2020-02-24 14:18:07 +0200

    23

--- src/components/PizzaList/PizzaItem/index.vue
+++ src/components/PizzaList/PizzaItem/index.vue
@@ -1,16 +1,9 @@
 <template>
   <div>
     <h2>{{ pizza.name }}</h2>
-<<<<<<< HEAD
-    <img src="@/assets/images/1.jpg" />
-    <h4>Ціна: {{ pizza.price }}грн</h4>
-    <img :src="imgPath" />
-    <!-- <img src=`require(../assets/images/${id}.jpg)` /> -->
-=======
     <!-- <img :src="imgPath" />
     <img src="@/assets/images/1.jpg" />
     <img src=`require(../assets/images/${id}.jpg)` /> -->
->>>>>>> 7ab4c5a9da13d34254ffc0fcbdc4ec79b022648c
     <li v-for="(value, name) in pizza.ingredients" :key="name">{{name}}</li>
   </div>
 </template>
