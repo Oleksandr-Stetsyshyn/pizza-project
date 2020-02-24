d09bb0b7faf0cf7fb9a970ed24b14327803e8c60
oleksandr_stetsyshyn <56292371+oleksandrstetsyshyn@users.noreply.github.com>
2020-02-24 18:16:30 +0200

    j

--- src/components/PizzaList/PizzaItem/index.vue
+++ src/components/PizzaList/PizzaItem/index.vue
@@ -4,33 +4,29 @@
     <img :src="imgPath" />
     {{pizza.id}}
     <!-- <img src="@/assets/images/1.jpg" /> 
-    <img :src="require(`@/assets/images/${id}.jpg`)" /> -->
+    <img :src="require(`@/assets/images/${id}.jpg`)" />-->
     <li v-for="(value, name) in pizza.ingredients" :key="name">{{name}}</li>
   </div>
 </template>
 
 <script>
 export default {
-  name: "PizzaItem", 
+  name: "PizzaItem",
 
   props: {
     pizza: {
       type: Object
-    },
-
-
-  },
-      computed: {
-      imgPath() {
-        // console.log('this.pizza.id');
-        // console.log(this.pizza.id);
-        
-        return require(`@/assets/images/${this.pizza.id}.jpg`); //немтудом
-      }
     }
+  },
+  computed: {
+    imgPath() {
+      // console.log('this.pizza.id');
+      // console.log(this.pizza.id);
+      return require(`@/assets/images/${this.pizza.id}.jpg`);
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
